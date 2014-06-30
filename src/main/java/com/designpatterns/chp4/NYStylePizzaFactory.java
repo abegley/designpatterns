e0e3c409a7442e68f409e3f41e86fe70aa42b6af
Alan Begley <begleyalan@gmail.com>
2014-06-30 20:13:53 +0100

    renamed NYStylePizzaStore.java to NYStylePizzaFactory.java

--- src/main/java/com/designpatterns/chp4/NYStylePizzaFactory.java
+++ src/main/java/com/designpatterns/chp4/NYStylePizzaFactory.java
@@ -1,6 +1,6 @@
 package com.designpatterns.chp4;
 
-public class NYStylePizzaStore extends PizzaStore {
+public class NYStylePizzaFactory extends PizzaStore {
     @Override
     Pizza createPizza(String type) {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
